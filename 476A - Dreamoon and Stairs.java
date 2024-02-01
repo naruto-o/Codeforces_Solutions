c03056a58718223e732d50372f5f8eaa118c8ab7
Shikhar Sharma <73755581+naruto-o@users.noreply.github.com>
2024-02-01 23:14:03 +0530

    Create 476A - Dreamoon and Stairs.java

---
+++ 476A - Dreamoon and Stairs.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class dreamoonstairs{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int x = 0;
+        if(m > n){
+            x = -1;
+        }
+        else{
+            if(n%2==0){
+                x = n/2;
+            }else{
+                x = (n/2)+1;
+            }
+            while(x%m != 0){
+                x++;
+            }
+        }
+        System.out.println(x);
+    }
+}
